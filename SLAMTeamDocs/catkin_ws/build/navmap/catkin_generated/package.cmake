cf14ba740be0932a8b43dd538d70cc0fd782dd6f
meyerat <alex.t.meyer@vanderbilt.edu>
2014-04-11 19:09:31 -0500

    Adjusted code that splits up, down, norm

---
+++ SLAMTeamDocs/catkin_ws/build/navmap/catkin_generated/package.cmake
@@ -0,0 +1,7 @@
+set(_CATKIN_CURRENT_PACKAGE "navmap")
+set(navmap_MAINTAINER "NavMapLeader <alex.t.meyer@vanderbilt.edu>")
+set(navmap_DEPRECATED "")
+set(navmap_VERSION "0.0.0")
+set(navmap_BUILD_DEPENDS "hokuyo_node" "roscpp" "sensor_msgs" "std_msgs")
+set(navmap_RUN_DEPENDS "hokuyo_node" "roscpp" "sensor_msgs" "std_msgs")
+set(navmap_BUILDTOOL_DEPENDS "catkin")
---
+++ SLAMTeamDocs/catkin_ws/build/navmap/cmake_install.cmake
@@ -0,0 +1,49 @@
+# Install script for directory: /home/UAVTeam/catkin_ws/src/navmap
+
+# Set the install prefix
+IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  SET(CMAKE_INSTALL_PREFIX "/home/UAVTeam/catkin_ws/install")
+ENDIF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+STRING(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+IF(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  IF(BUILD_TYPE)
+    STRING(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  ELSE(BUILD_TYPE)
+    SET(CMAKE_INSTALL_CONFIG_NAME "")
+  ENDIF(BUILD_TYPE)
+  MESSAGE(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+ENDIF(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+
+# Set the component getting installed.
+IF(NOT CMAKE_INSTALL_COMPONENT)
+  IF(COMPONENT)
+    MESSAGE(STATUS "Install component: \"${COMPONENT}\"")
+    SET(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  ELSE(COMPONENT)
+    SET(CMAKE_INSTALL_COMPONENT)
+  ENDIF(COMPONENT)
+ENDIF(NOT CMAKE_INSTALL_COMPONENT)
+
+# Install shared libraries without execute permission?
+IF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  SET(CMAKE_INSTALL_SO_NO_EXE "1")
+ENDIF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+
+IF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  FILE(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "/home/UAVTeam/catkin_ws/build/navmap/catkin_generated/installspace/navmap.pc")
+ENDIF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+
+IF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  FILE(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/navmap/cmake" TYPE FILE FILES
+    "/home/UAVTeam/catkin_ws/build/navmap/catkin_generated/installspace/navmapConfig.cmake"
+    "/home/UAVTeam/catkin_ws/build/navmap/catkin_generated/installspace/navmapConfig-version.cmake"
+    )
+ENDIF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+
+IF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  FILE(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/navmap" TYPE FILE FILES "/home/UAVTeam/catkin_ws/src/navmap/package.xml")
+ENDIF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+
